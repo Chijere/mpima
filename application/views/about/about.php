7ad997c016ef908261fb1faea111e7fec7b29cef
chijere <chawchijere@gmail.com>
2018-09-02 15:46:45 +0200

    change from mpico to mpima

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -79,7 +79,7 @@
 				<div class="col-lg-7 order-lg-1 order-2">
 					<h2 class="intro_title">MPIMA Investments</h2>
 					<div class="intro_subtitle">Build a better Malawi with high quality standards and designs at cheap rates</div>
-					<p class="intro_text">MPICO Investments designs modern building designs, house plans, monitor, control, operate oversee and account for property, survey sites and establishes the market value of urban and rural area lands</p>
+					<p class="intro_text">MPIMA Investments designs modern building designs, house plans, monitor, control, operate oversee and account for property, survey sites and establishes the market value of urban and rural area lands</p>
 					<div class="button intro_button trans_200"><a class="trans_200" href="#">read more</a></div>
 				</div>
 			</div>
